85c8da258fc9ce68ee9853f06d5462f4e8b4134d
GustavFluur <gustavfluur@hotmail.com>
2022-05-02 19:41:56 +0000

    Add of if, elife & else statements in the file

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 number = 5 
 print('Try to guess the number (between 1 and 14) in three guesses!')
 
-guess1 = int(input('\Enter your first guess: '))
-guess2 = int(input('\Enter your second guess: '))
-guess3 = int(input('\Enter your third guess: '))
+try1 = int(input('\Enter your first guess: '))
+if try1 == number:
+    print('On the first try you scored the right one, what is your secret!?')
+elif try1 > number:
+    print('Your guess is too high, please try again!')
+try2 = int(input('\Enter your second guess: '))
+try3 = int(input('\Enter your third guess: '))
