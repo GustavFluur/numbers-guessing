3c4338a6a57ef715647f9a57f0349c4b0850de36
GustavFluur <gustavfluur@hotmail.com>
2022-05-02 19:35:41 +0000

    Add new change to another spelling error within the file

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ print('Try to guess the number (between 1 and 14) in three guesses!')
 
 guess1 = int(input('\Enter your first guess: '))
 guess2 = int(input('\Enter your second guess: '))
-guess3 = int(input('\Enter your thrid guess: '))
+guess3 = int(input('\Enter your third guess: '))
