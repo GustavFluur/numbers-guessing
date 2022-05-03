28655d17ec4b283eb0782769b1bf8e8f44d44f1f
GustavFluur <gustavfluur@hotmail.com>
2022-05-03 11:22:34 +0000

    Add new if, else statements within input element

--- run.py
+++ run.py
@@ -1,10 +1,19 @@
-number = 5 
-print('Try to guess the number (between 1 and 14) in three guesses!')
+number = 10 
+print('Try to guess the number (between 0 and 50) in three guesses!')
 
 try1 = int(input('\Enter your first guess: '))
 if try1 == number:
     print('On the first try you scored the right one, what is your secret!?')
 elif try1 > number:
     print('Your guess is too high, please try again!')
+else: 
+    print('Almost there, number to low!')
 try2 = int(input('\Enter your second guess: '))
+try1 = int(input('\Enter your first guess: '))
+if try1 == number:
+    print('On the first try you scored the right one, what is your secret!?')
+elif try1 > number:
+    print('Your guess is too high, please try again!')
+else: 
+    print('Almost there, number to low!')
 try3 = int(input('\Enter your third guess: '))
