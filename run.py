2c0d8d67575189b8b947ed7fe1b8b85deb2f6b5b
GustavFluur <gustavfluur@hotmail.com>
2022-05-03 12:20:16 +0000

    Add more and new content into the try3 element

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 number = 10 
-print('Try to guess the number (between 0 and 50) in three guesses!')
+print('Try to guess the number (between 0 and 100) in three guesses!')
 
+"""
+Here the user is typing in the number and to succeed in this game, you must choose the right one.
+How you are doing that is by selecting a number between 0 - 100 for creating some challenge to the player. 
+Once you type into a number you reccieve a message into the terminal or the game to understand if you are on the right path.
+"""
 try1 = int(input('\Enter your first guess: '))
 if try1 == number:
     print('On the first try you scored the right one, what is your secret!?')
@@ -12,10 +17,17 @@ else:
 try2 = int(input('\Enter your second guess: '))
 
 if try2 == number:
-    print('On the first try you scored the right one, what is your secret!?')
+    print('Great, you made it on the second try!')
 elif try2 > number:
     print('Your guess is too high, please try again!')
 else: 
     print('Almost there, number to low!')
-    
+
 try3 = int(input('\Enter your third guess: '))
+
+if try3 == number:
+    print('Great, you made it on the second try!')
+elif try3 > number:
+    print('Your guess is too high, please try again!')
+else: 
+    print('Almost there, number to low!')
