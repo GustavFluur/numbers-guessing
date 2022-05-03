2c30801490d0b7c21c31f24709269910f94e6d8e
GustavFluur <gustavfluur@hotmail.com>
2022-05-03 16:21:09 +0000

    Change of spelling and change of content with in the file

--- run.py
+++ run.py
@@ -1,14 +1,14 @@
 import sys
 import random
 
-number = random.randint(1, 100)
-print('Try to guess the number (between 1 and 100) on three guesses!')
+number = random.randint(1, 12)
+print('Try to guess the number (between 0 and 12) on four guesses!')
 
 """Here the user is typing in the number and to succeed in this game, you must choose the right one. How you are doing that is by selecting a number between 0 - 20."""
 
 try1 = int(input('\Enter your first guess: '))
 if try1 == number:
-    print('On the first try you scored the right one, what is your secret!?')
+    print('On the first try you scored the right one, what is your secret!?'.upper())
     sys.exit()
 elif try1 > number:
     print('Your guess is too high, please try again!')
@@ -18,7 +18,7 @@ else:
 try2 = int(input('\Enter your second guess: '))
 
 if try2 == number:
-    print('Great, you made it on the second try!')
+    print('Great, you made it on the second try!'.upper())
     sys.exit()
 elif try2 > number:
     print('Your guess is too high, please try again!')
@@ -28,9 +28,19 @@ else:
 try3 = int(input('\Enter your third guess: '))
 
 if try3 == number:
-    print('Correct number, nice one!')
+    print('Correct number after three guesses, nice one!')
     sys.exit()
 elif try3 > number:
+    print('Your guess is too high, please try again!')
+else: 
+    print('Almost there, the number too low!')
+
+try4 = int(input('\Enter your fourth guess: '))
+
+if try4 == number:
+    print('Correct number on the last try, well done!')
+    sys.exit()
+elif try4 > number:
     print('Your guess is too high, please try again!')
 else: 
     print('Almost there, the number too low!')
