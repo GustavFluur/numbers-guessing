87cc0cbf0e3f23610761d8cfd54efae0654307af
GustavFluur <gustavfluur@hotmail.com>
2022-05-04 08:38:02 +0000

    re-deploy Heroku

--- run.py
+++ run.py
@@ -4,8 +4,6 @@ import random
 number = random.randint(1, 12)
 print('Try to guess the number (between 0 and 12) on four guesses!')
 
-"""Here the user is typing in the number and to succeed in this game, you must choose the right one. How you are doing that is by selecting a number between 0 - 20."""
-
 try1 = int(input('\Enter your first guess: '))
 if try1 == number:
     print('On the first try you scored the right one, what is your secret!?'.upper())
