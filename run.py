0f8c123eb0926dbed48800e4fa5b291b46e00458
GustavFluur <gustavfluur@hotmail.com>
2022-05-04 12:51:25 +0000

    Change regarding the chances from four to five and another if statement

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import sys
 import random
 
 number = random.randint(1, 12)
-print('Try to guess the number (between 0 and 12) on four guesses!')
+print('Try to guess the number (between 0 and 12) on five guesses!')
 
 try1 = int(input('Enter your first guess: '))
 if try1 == number:
@@ -13,7 +13,7 @@ elif try1 > number:
 else: 
     print('Almost there, the number too low!')
 
-try2 = int(input('\Enter your second guess: '))
+try2 = int(input('Enter your second guess: '))
 
 if try2 == number:
     print('Great, you made it on the second try!'.upper())
@@ -23,7 +23,7 @@ elif try2 > number:
 else: 
     print('Almost there, the number too low!')
 
-try3 = int(input('\Enter your third guess: '))
+try3 = int(input('Enter your third guess: '))
 
 if try3 == number:
     print('Correct number after three guesses, nice one!')
@@ -33,12 +33,22 @@ elif try3 > number:
 else: 
     print('Almost there, the number too low!')
 
-try4 = int(input('\Enter your fourth guess: '))
+try4 = int(input('Enter your fourth guess: '))
 
 if try4 == number:
     print('Correct number on the last try, well done!')
     sys.exit()
 elif try4 > number:
+    print('Your guess is too high, please try again!')
+else: 
+    print('Almost there, the number too low!')
+
+try5 = int(input('Enter your fourth guess: '))
+
+if try5 == number:
+    print('You made it eventually, great work!')
+    sys.exit()
+elif try5 > number:
     print('Your guess is too high, please try again!')
 else: 
     print('Almost there, the number too low!')
