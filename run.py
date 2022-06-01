22ecc467fb8f3c2b74433658b631cf6fde0719a8
GustavFluur <gustavfluur@hotmail.com>
2022-06-01 19:31:53 +0000

    fixed bug with entering anything other than number

--- run.py
+++ run.py
@@ -8,64 +8,27 @@ where structure is based upon on one variable with a few if statements.
 -No difficulties to enhance it further.
 """
 
-def guess(number):
-    try_input = int(input('Enter your first guess: '))
-    if try_input == number:
-        print('Wow, you scored on the first try! How did you know!?'.upper())
+def guess(i, correct_number):
+    try:
+        try_number = int(input('Enter guess ' + str(i) + ': '))
+    except ValueError:
+        print ('Please enter a number!')
+        guess(i, correct_number)
+
+    if try_number == correct_number:
+        print('Wow, you scored on try' + str(i) + '! How did you know!?'.upper())
         print('Shuting down..')
         sys.exit()
-    elif try_input > number:
+    elif try_number > correct_number:
         print('Your guess is too high, please try again!')
     else:
         print('Almost there, the number too low!')
 
 number = random.randint(1, 100)
 print('Try to guess the number (between 0 and 100) on five guesses!')
-guess(number)
 
-try2 = int(input('Enter your second guess: '))
-
-if try2 == number:
-    print('Great, you made it on the second try!'.upper())
-    print('Shuting down..')
-    sys.exit()
-elif try2 > number:
-    print('Your guess is too high, please try again!')
-else:
-    print('Almost there, the number too low!')
-
-try3 = int(input('Enter your third guess: '))
-
-if try3 == number:
-    print('Correct number after three guesses, nice one!')
-    print('Shuting down..')
-    sys.exit()
-elif try3 > number:
-    print('Your guess is too high, please try again!')
-else:
-    print('Almost there, the number too low!')
-
-try4 = int(input('Enter your fourth guess: '))
-
-if try4 == number:
-    print('Correct number on the last try, well done!')
-    print('Shuting down..')
-    sys.exit()
-elif try4 > number:
-    print('Your guess is too high, please try again!')
-else:
-    print('Almost there, the number too low!')
-
-try5 = int(input('Enter your fifth guess: '))
-
-if try5 == number:
-    print('You made it eventually, great work!')
-    print('Shuting down..')
-    sys.exit()
-elif try5 > number:
-    print('Your guess is too high, please try again!')
-else:
-    print('Almost there, the number too low!')
+for i in [1, 2, 3,4, 5]:
+    guess(i, number)
 
 print('No more chances, you lost the game....')
 print('Shuting down..')
